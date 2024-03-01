fd083e87f7123beabf09bb6de778afa194935985
Vlad <vlad.mocanu13@gmail.com>
2024-03-01 17:34:22 +0200

    - small changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Quizz from './components/Quizz'
 import React from 'react'
-import { nanoid } from 'nanoid'
 import Intro from './components/Intro'
 
 
@@ -17,12 +16,14 @@ function App() {
      fetch("https://opentdb.com/api.php?amount=5&type=multiple")
           .then(res => res.json())
           .then(data => setQuestions(data.results))
+          .catch(error => { throw error })
   }, [])
   
   function newQuestions() {
       fetch("https://opentdb.com/api.php?amount=5&type=multiple")
           .then(res => res.json())
           .then(data => setQuestions(data.results))
+          .catch(error => { throw error })
       setCorrectAnswers(0)
       setAnsweredQuestions(0)
       const changeButtons = document.querySelectorAll('.answers button')
@@ -32,9 +33,9 @@ function App() {
       setChecked(false)
   }
   
-  const quizQuestions = questions.map(result => {
-          return (<Quizz result={result} question={questions}  answerQuestion={answerQuestion} />)
-  })
+  const quizQuestions = questions ? questions.map(result => {
+        return (<Quizz result={result} question={questions}  answerQuestion={answerQuestion} />)
+  }) : "Try Again!\n";
   
    function answerQuestion(event, value){
       setAnsweredQuestions(prev => prev + 1)
@@ -46,29 +47,32 @@ function App() {
   }
   
   function checkAnswers() {
-      const incorrectAnswers = document.querySelectorAll('button[value=incorrect]')
-      for (const answer of incorrectAnswers){
-          answer.className = 'btn-disable'
-      }
-      const correctAnswers = document.querySelectorAll('button[value=correct]')
-      for (const answer of correctAnswers){
-          answer.className = 'correctClicked'
-      }
-      setChecked(true)
+    const incorrectAnswers = document.querySelectorAll('button[value=incorrect]')
+    for (const answer of incorrectAnswers){
+        answer.className = 'btn-disable'
+    }
+    const correctAnswers = document.querySelectorAll('button[value=correct]')
+    for (const answer of correctAnswers){
+        answer.className = 'correctClicked'
+    }
+    setChecked(true)
   }
    
   return (
       <main>
-          {introState ? <div className='questions'><img className='blob1'src="./images/blob1.svg"/>
-          <img className='blob2'src="./images/blob2.svg"/><Intro startQuiz={startQuiz}/> </div> :
+          { introState ? <div className='questions'>
+            <img className='blob1' alt="" src='/images/blob1.svg' />
+            <img className='blob2' alt="" src='/images/blob2.svg'  />
+            <Intro startQuiz={startQuiz}/> 
+          </div> :
           <div className='questions'>
-              <img className='blob1-start'src="./images/blob1.svg"/>
-              <img className='blob2-start'src="./images/blob2.svg"/>
+              <img className='blob1-start' src='/images/blob1.svg' alt="" />
+              <img className='blob2-start' src='/images/blob2.svg' alt="" />
               {quizQuestions}
               {answeredQuestions === 5 ? <div class='score'><h2 className='finalScore' >You have Scored {correctAnswers}/5</h2><button className='qbutton' onClick={newQuestions}>Play Again</button></div> : checked === false ? <button className='qbutton' onClick={checkAnswers}>Check Answers</button> : <button className='qbutton' onClick={newQuestions}>Play Again</button>}
-          </div>}
+          </div> }
       </main>  
-      )
+    )
 }
 
-export default App;
+export default App; 
